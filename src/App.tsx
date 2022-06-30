f867552aa640453fd66fd169b90a78c1438b4996
Joao Vitor <joaovitorsocial25@gmail.com>
2022-06-30 16:23:27 -0300

    Criando pagina Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "./components/paginas/login/Login";
 
 export default function App() {
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
       <div style={{ minHeight: '100vh' }}>
         <Routes> // Antigo Switch
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          {/*<Route path="/cadastro" element={<CadastroUsuario />} />*/}
+          {/* <Route path="/home" element={<Home />} /> */}
+          {/* {<Route path="/cadastro" element={<CadastroUsuario />} />} */}
         </Routes>
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </Router>
+
   )
 }
