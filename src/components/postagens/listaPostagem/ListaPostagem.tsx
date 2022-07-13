b7a1b736c50dda6b622249647835752b6ba5caf4
Joao Vitor <joaovitorsocial25@gmail.com>
2022-07-13 09:04:15 -0300

    adicioando o nome do criador na postagem

--- src/components/postagens/listaPostagem/ListaPostagem.tsx
+++ src/components/postagens/listaPostagem/ListaPostagem.tsx
@@ -8,77 +8,74 @@ import { buscar } from "../../../services/Service";
 import { TokenState } from "../../../store/tokens/tokenReducer";
 
 export default function ListaPostagem() {
-    const [posts, setPosts] = useState<Postagem[]>([])
-    const token = useSelector<TokenState, TokenState["token"]>(
-      (state) => state.token
-    );
-    let navigate = useNavigate();
-  
-    useEffect(() => {
-      if (token == "") {
-        alert("Você precisa estar logado")
-        navigate("/login")
-  
-      }
-    }, [token])
-  
-    async function getPost() {
-      await buscar("/postagem", setPosts, {
-        headers: {
-          'Authorization': token
-        }
-      })
+  const [posts, setPosts] = useState<Postagem[]>([])
+  const token = useSelector<TokenState, TokenState["token"]>(
+    (state) => state.token
+  );
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (token === "") {
+      alert("Você precisa estar logado")
+      navigate("/login")
+
     }
-  
-    useEffect(() => {
-  
-      getPost()
-  
-    }, [posts.length])
-  
-    return (
-      <>
-        {
-          posts.map(post => (
-            <Box m={2} >
-              <Card variant="outlined">
-                <CardContent>
-                  <Typography color="textSecondary" gutterBottom>
-                    Postagens
-                  </Typography>
-                  <Typography variant="h5" component="h2">
-                    {post.titulo}
-                  </Typography>
-                  <Typography variant="body2" component="p">
-                    {post.mensagem}
-                  </Typography>
-                  <Typography variant="body2" component="p">
-                    {post.tema?.tema}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Box display="flex" justifyContent="center" mb={1.5}>
-  
-                    <Link to={`/formularioPostagem/${post.id}`} className="text-decorator-none" >
-                      <Box mx={1}>
-                        <Button variant="contained" className="marginLeft" size='small' color="primary" >
-                          atualizar
-                        </Button>
-                      </Box>
-                    </Link>
-                    <Link to={`/deletarPostagem/${post.id}`} className="text-decorator-none">
-                      <Box mx={1}>
-                        <Button variant="contained" size='small' color="secondary">
-                          deletar
-                        </Button>
-                      </Box>
-                    </Link>
-                  </Box>
-                </CardActions>
-              </Card>
-            </Box>
-          ))
-        }
-      </>
-    )
+  }, [token]);
+
+  async function getPost() {
+    await buscar("/postagem", setPosts, {
+      headers: {
+        'Authorization': token
+      }
+    })
   }
+
+  useEffect(() => {
+
+    getPost()
+
+  }, [posts.length]);
+
+  return (
+    <>
+      {
+        posts.map(post => (
+          <Box m={3} >
+            <Card variant="outlined">
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>Postagens</Typography>
+                <Typography variant="h5" component="h2">{post.titulo}</Typography>
+                <Typography variant="body2" component="p">{post.mensagem}</Typography>
+                <Typography variant="body2" component="p">{post.tema?.tema}</Typography>
+                <Typography variant="body1" component="p">
+                  <Box marginTop={1.5}>
+                    Criado por: {post.usuario?.nome}
+                  </Box>
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Box display="flex" justifyContent="center" mb={1.5}>
+
+                  <Link to={`/formularioPostagem/${post.id}`} className="text-decorator-none" >
+                    <Box mx={1}>
+                      <Button variant="contained" className="marginLeft" size='small' color="primary" >
+                        atualizar
+                      </Button>
+                    </Box>
+                  </Link>
+                  <Link to={`/deletarPostagem/${post.id}`} className="text-decorator-none">
+                    <Box mx={1}>
+                      <Button variant="contained" size='small' color="secondary">
+                        deletar
+                      </Button>
+                    </Box>
+                  </Link>
+                </Box>
+              </CardActions>
+            </Card>
+          </Box>
+        ))
+      }
+    </>
+  )
+}
