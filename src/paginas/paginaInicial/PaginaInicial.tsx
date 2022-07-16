297ec1cafb5dd0c5762d22cdd659a01e5afbf225
joaolmb <joaolmb2@gmail.com>
2022-07-16 16:49:07 -0300

    alterações na pagina inicial

--- src/paginas/paginaInicial/PaginaInicial.tsx
+++ src/paginas/paginaInicial/PaginaInicial.tsx
@@ -10,6 +10,13 @@ import GithubIcon from '@material-ui/icons/GitHub';
 // Imagens:
 import joaoLima from '../../components/imagens/joaoLima.png';
 import humanos from '../../components/imagens/humanos.png';
+import joaoLucas from '../../components/imagens/joaoLucas.png';
+import pessoa3d from '../../components/imagens/pessoa3d.png';
+import mui from '../../components/imagens/mui.png';
+import react from '../../components/imagens/react.png';
+import java from '../../components/imagens/JAVA.png';
+import spring from '../../components/imagens/SPRINGBOOT.png';
+import matheus from '../../components/imagens/matheus.png';
 
 // CSS:
 import "./PaginaInicial.css";
@@ -36,14 +43,7 @@ export default function PaginaInicial() {
                         <Link to="/login" className="text-decoration-none">
                             <Box mx={1.5} className="cursor">
                                 <Typography variant="h6" color="inherit">
-                                    Quem somos?
-                                </Typography>
-                            </Box>
-                        </Link>
-                        <Link to="/login" className="text-decoration-none">
-                            <Box mx={1.5} className="cursor">
-                                <Typography variant="h6" color="inherit">
-                                    Nossos valores
+                                    Nossos pilares
                                 </Typography>
                             </Box>
                         </Link>
@@ -91,7 +91,7 @@ export default function PaginaInicial() {
                         </Grid>
                         <Grid item xs={7}>
                             <Typography className="texto">
-                                A ConectaONG é a rede social que tem como objetivo combater as desigualdades por meio do auxilio à Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam repellendus maiores sint voluptates, non dolores repellat laboriosam, voluptatibus quasi ut cupiditate nostrum qui asperiores ipsam. Consectetur cupiditate temporibus modi sed.
+                                A ConectaONG é a rede social que tem como objetivo combater as desigualdades por meio do auxilio à |||| A alta desigualdade na distribuição de renda no Brasil é um problema que aflige grande parte da população. Este fator se torna muito visível quando analisamos o coeficiente de Gini - indice utilizado para aferir a desigualdade econômica de um determinado local. De acordo com o último levantamento desse indice realizado em 2019, o Brasil encontra-se na 9ª colocação mundial integralizando o grupo de países que possuem maiores desigualdades e concentrações de renda.
                             </Typography>
                         </Grid>
                     </Paper>
@@ -99,40 +99,25 @@ export default function PaginaInicial() {
 
                 {/* Infos sobre a ConectaONG */}
                 <Grid container xs={12} spacing={5} className="grid-info">
-                    <Grid item xs={12}></Grid>
-                    <Grid item md={6}>
-                        <Typography className="text-info">
-                            A alta desigualdade na distribuição de renda no Brasil é um problema que aflige grande parte da população. Este fator se torna muito visível quando analisamos o coeficiente de Gini - indice utilizado para aferir a desigualdade econômica de um determinado local. De acordo com o último levantamento desse indice realizado em 2019, o Brasil encontra-se na 9ª colocação mundial integralizando o grupo de países que possuem maiores desigualdades e concentrações de renda. Tendo em vista esse cenário, nosso grupo escolheu o Objetivo de Desenvolvimento Sustentável (ODS) 10: Redução as desigualdades.
+                    
+                    <Grid item md={6} xs={12}>
+                        <Typography className="texto">
+                            Por isso temos como base a <a href="">ODS - 10 (Redução das Desigualdades)</a>  Acreditamos que essa ODS tenha um papel realmente significativo no país, até porque as desigualdades socioeconômicas não são as únicas que enfrentamos. Atualmente ainda é muito visível preconceitos raciais, regionais e de gênero, muitos deles estruturados na nossa sociedade desde seu princípio e isso manifesta desacordo com o artigo 1 da Declaração Universal dos Direitos Humanos: "Todos os seres humanos nascem livres e iguais em dignidade e direitos. São dotados de razão e consciência e devem agir em relação uns aos outros com espirito de fraternidade".
+
                         </Typography>
                     </Grid>
+
                     <Grid item md={6} xs={12}>
-                        <img src={humanos} alt="" className="imagem-info" />
+                        <img src={pessoa3d} alt="" className="imagem-info" />
                     </Grid>
+
                     <Grid item md={6} xs={12}>
                         <img src="https://images.pexels.com/photos/12739401/pexels-photo-12739401.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="imagem-info" />
                     </Grid>
                     <Grid item md={6} xs={12}>
-                        <Typography>
-                            Acreditamos que essa ODS tenha um papel realmente significativo no país, até porque as desigualdades socioeconômicas não são as únicas que enfrentamos. Atualmente ainda é muito visível preconceitos raciais, regionais e de gênero, muitos deles estruturados na nossa sociedade desde o início de nossa colonização. E isso manifesta desacordo com o artigo 1 da Declaração Universal dos Direitos Humanos: "Todos os seres humanos nascem livres e iguais em dignidade e direitos. São dotados de razão e consciência e devem agir em relação uns aos outros com espirito de fraternidade".
-
-                        </Typography>
-                    </Grid>
-
-                    <Grid item md={6} xs={12}>
-                        <Typography>
+                        <Typography className="texto">
                             Objetivo do ConectaONG:
-                            Dessa forma decidimos em conjunto que a melhor maneira de combater essas desigualdades seria auxiliar de alguma maneira as diversas ONGs já trabalham com esse intuito. Por isso decidimos criar a ConectaONG, uma rede social que visa divulgar o trabalho dessas instituições que muitas vezes não são conhecidas e garantir maior visibilidade para essas causas.
-                        </Typography>
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                        <img src="https://images.pexels.com/photos/12739401/pexels-photo-12739401.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="imagem-info" />
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                        <img src="https://images.pexels.com/photos/12739401/pexels-photo-12739401.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="imagem-info" />
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                        <Typography>
-                            Como:
+                            Dessa forma decidimos em conjunto que a melhor maneira de combater essas desigualdades seria auxiliar de alguma maneira as diversas ONGs já trabalham com esse intuito. Por isso decidimos criar a ConectaONG, uma rede social que visa divulgar o trabalho dessas instituições que muitas vezes não são conhecidas e garantir maior visibilidade para essas causas. Como:
                             Temos a ideia de criar uma plataforma onde as ONGs, ou representantes das mesmas fizessem um cadastro apresentando seus objetivos, projetos, integrantes, e uma forma de contato para que pessoas físicas e jurídicas possam colaborar de alguma maneira. De tal modo a possibilitar interação entre usuários e ONGs.
                         </Typography>
                     </Grid>
@@ -141,14 +126,14 @@ export default function PaginaInicial() {
                 {/* Valores */}
                 <Grid container md={12} className="valores">
                     <Grid item md={12}>
-                        <Typography className="subtitulos">Nossos Valores</Typography>
+                        <Typography className="subtitulos">Nossos Pilares</Typography>
                     </Grid>
                     <Grid item md={3} className="cartao-valores" >
                         <Typography className="nome-valor">
-                            Valores?
+                            Visão
                         </Typography>
                         <Box>
-                            <img src="https://cdn-icons-png.flaticon.com/512/226/226777.png" alt="" className="foto-tecnologia zoom" />
+                            <img src="https://cdn-icons-png.flaticon.com/512/709/709612.png" alt="" className="foto-tecnologia zoom" />
                         </Box>
                         <Typography>
                             é uma boa pergunta
@@ -156,10 +141,10 @@ export default function PaginaInicial() {
                     </Grid>
                     <Grid item md={3} className="cartao-valores" >
                         <Typography className="nome-valor">
-                            Eu realmente não sei
+                            Missão
                         </Typography>
                         <Box>
-                            <img src="https://cdn-icons-png.flaticon.com/512/226/226777.png" alt="" className="foto-tecnologia zoom" />
+                            <img src="https://cdn-icons-png.flaticon.com/512/102/102349.png" alt="" className="foto-tecnologia zoom" />
                         </Box>
                         <Typography>
                             o que poderiamos
@@ -167,10 +152,10 @@ export default function PaginaInicial() {
                     </Grid>
                     <Grid item md={3} className="cartao-valores" >
                         <Typography className="nome-valor">
-                            colocar
+                            Valores
                         </Typography>
                         <Box>
-                            <img src="https://cdn-icons-png.flaticon.com/512/226/226777.png" alt="" className="foto-tecnologia zoom" />
+                            <img src="https://cdn-icons-png.flaticon.com/512/2285/2285606.png" alt="" className="foto-tecnologia zoom" />
                         </Box>
                         <Typography>
                             nesse tópico
@@ -190,7 +175,7 @@ export default function PaginaInicial() {
                             JAVA
                         </Typography>
                         <Box>
-                            <img src="https://cdn-icons-png.flaticon.com/512/226/226777.png" alt="" className="foto-tecnologia zoom" />
+                            <img src={java} alt="" className="foto-tecnologia zoom" />
                         </Box>
                         <Typography>
                             Java é uma linguagem de programação e plataforma de computação liberada pela primeira vez pela Sun Microsystems em 1995. De um início humilde, ela evoluiu para uma grande participação no mundo digital dos dias atuais, oferecendo a plataforma confiável na qual muitos serviços e aplicativos são desenvolvidos.
@@ -201,8 +186,7 @@ export default function PaginaInicial() {
                             Spring Boot
                         </Typography>
                         <Box>
-                            {/* A imagem na verdade é do Spring Boot */}
-                            <img src="https://devkico.itexto.com.br/wp-content/uploads/2014/08/spring-boot-project-logo.png" alt="" className="foto-tecnologia zoom" />
+                            <img src={spring} alt="" className="foto-tecnologia zoom" />
                         </Box>
                         <Typography>
                             O Spring é um framework open source para a plataforma Java criado por Rod Johnson e descrito em seu livro "Expert One-on-One: JEE Design e Development". Trata-se de um framework não intrusivo, baseado nos padrões de projeto inversão de controle e injeção de dependência.
@@ -213,7 +197,7 @@ export default function PaginaInicial() {
                             React.js
                         </Typography>
                         <Box>
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt="" className="foto-tecnologia zoom" />
+                            <img src={react} alt="" className="foto-tecnologia zoom" />
                         </Box>
                         <Typography>
                             É uma biblioteca JavaScript para criação de interfaces para o usuário, desenvolvida e mantida pelo Facebook, sua primeira release saiu em 2013. Conta com mais de mil colaboradores ativos no GitHub. Ele está presente no nosso dia-a-dia em empresas grandes como Facebook, Instagram, AirBnB, NFL, Yahoo e muito mais.
@@ -224,7 +208,7 @@ export default function PaginaInicial() {
                             Material UI
                         </Typography>
                         <Box>
-                            <img src="https://mui.com/static/logo.png" alt="" className="foto-tecnologia zoom" />
+                            <img src={mui} alt="" className="foto-tecnologia zoom" />
                         </Box>
                         <Typography>
                             Uma das populares bibliotecas de componentes React. Considerada por muitos como a melhor biblioteca de uso geral do mercado. Ela segue as diretrizes, componentes e ferramentas de 'material design' do Google para tornar os aplicativos da web mais rápidos.
@@ -275,7 +259,7 @@ export default function PaginaInicial() {
                     </Grid>
                     <Grid item md={2} className="cartao-time" >
                         <Box>
-                            <img src="https://avatars.githubusercontent.com/u/97589444?v=4" alt="" className="foto-time zoom" />
+                            <img src={joaoLucas} alt="" className="foto-time zoom" />
                             <Typography className="nome-time">
                                 João Marques
                             </Typography>
@@ -292,7 +276,6 @@ export default function PaginaInicial() {
                     <Grid item md={2} className="cartao-time" >
                         <Box>
                             <img src={joaoLima} alt="" className="foto-time zoom"/>
-                            {/* <img src="https://avatars.githubusercontent.com/u/100228980?v=4" alt="" className="foto-time zoom" /> */}
                             <Typography className="nome-time">
                                 João Lima
                             </Typography>
@@ -308,7 +291,7 @@ export default function PaginaInicial() {
                     </Grid>
                     <Grid item md={2} className="cartao-time" >
                         <Box>
-                            <img src="https://c.tenor.com/nEsdZ0qa6QcAAAAd/dog.gif" alt="" className="foto-time zoom" />
+                            <img src={matheus} alt="" className="foto-time zoom" />
                             <Typography className="nome-time">
                                 Matheus Chaves
                             </Typography>
